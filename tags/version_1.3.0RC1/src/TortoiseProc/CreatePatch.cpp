8f71ae00031fec29d4549f89fb7e5f3e668709cd
tortoisesvn <tortoisesvn@4cfb86a4-2c7a-11df-bd14-c1e331e6ceac>
2005-12-30 11:58:11 +0000

    Create the 1.3.0RC1 tag

---
+++ tags/version_1.3.0RC1/src/TortoiseProc/CreatePatch.cpp
@@ -0,0 +1,221 @@
+// CreatePatch.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "TortoiseProc.h"
+#include "MessageBox.h"
+#include "CreatePatch.h"
+#include "SVN.h"
+#include ".\createpatch.h"
+
+
+// CCreatePatch dialog
+
+IMPLEMENT_DYNAMIC(CCreatePatch, CResizableStandAloneDialog)
+CCreatePatch::CCreatePatch(CWnd* pParent /*=NULL*/)
+	: CResizableStandAloneDialog(CCreatePatch::IDD, pParent),
+	m_bThreadRunning(false)
+{
+}
+
+CCreatePatch::~CCreatePatch()
+{
+}
+
+void CCreatePatch::DoDataExchange(CDataExchange* pDX)
+{
+	CResizableStandAloneDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_PATCHLIST, m_PatchList);
+	DDX_Control(pDX, IDC_SELECTALL, m_SelectAll);
+}
+
+
+BEGIN_MESSAGE_MAP(CCreatePatch, CResizableStandAloneDialog)
+	ON_BN_CLICKED(IDC_SELECTALL, OnBnClickedSelectall)
+	ON_BN_CLICKED(IDHELP, OnBnClickedHelp)
+END_MESSAGE_MAP()
+
+
+// CCreatePatch message handlers
+
+BOOL CCreatePatch::OnInitDialog()
+{
+	CResizableStandAloneDialog::OnInitDialog();
+
+	//set the listcontrol to support checkboxes
+	m_PatchList.Init(0, SVNSLC_POPALL ^ (SVNSLC_POPIGNORE));
+	m_PatchList.SetSelectButton(&m_SelectAll);
+
+	AddAnchor(IDC_PATCHLIST, TOP_LEFT, BOTTOM_RIGHT);
+	AddAnchor(IDC_SELECTALL, BOTTOM_LEFT, BOTTOM_RIGHT);
+	AddAnchor(IDOK, BOTTOM_RIGHT);
+	AddAnchor(IDCANCEL, BOTTOM_RIGHT);
+	AddAnchor(IDHELP, BOTTOM_RIGHT);
+	if (hWndExplorer)
+		CenterWindow(CWnd::FromHandle(hWndExplorer));
+	EnableSaveRestore(_T("CreatePatchDlg"));
+
+	//first start a thread to obtain the file list with the status without
+	//blocking the dialog
+	if(AfxBeginThread(PatchThreadEntry, this) == NULL)
+	{
+		CMessageBox::Show(this->m_hWnd, IDS_ERR_THREADSTARTFAILED, IDS_APPNAME, MB_OK | MB_ICONERROR);
+	}
+	m_bThreadRunning = TRUE;
+
+	return TRUE;
+}
+
+UINT CCreatePatch::PatchThreadEntry(LPVOID pVoid)
+{
+	return ((CCreatePatch*)pVoid)->PatchThread();
+}
+UINT CCreatePatch::PatchThread()
+{
+	//get the status of all selected file/folders recursively
+	//and show the ones which have to be committed to the user
+	//in a listcontrol. 
+	GetDlgItem(IDOK)->EnableWindow(false);
+	GetDlgItem(IDCANCEL)->EnableWindow(false);
+
+	m_PatchList.GetStatus(m_pathList);
+	m_PatchList.Show(SVNSLC_SHOWUNVERSIONED | SVNSLC_SHOWDIRECTFILES | SVNSLC_SHOWVERSIONEDBUTNORMALANDEXTERNALS, 
+						SVNSLC_SHOWDIRECTFILES | SVNSLC_SHOWVERSIONEDBUTNORMALANDEXTERNALS);
+
+	GetDlgItem(IDOK)->EnableWindow(true);
+	GetDlgItem(IDCANCEL)->EnableWindow(true);
+	m_bThreadRunning = FALSE;
+	return 0;
+}
+
+BOOL CCreatePatch::PreTranslateMessage(MSG* pMsg)
+{
+	if (pMsg->message == WM_KEYDOWN)
+	{
+		switch (pMsg->wParam)
+		{
+		case VK_RETURN:
+			{
+				if (GetAsyncKeyState(VK_CONTROL)&0x8000)
+				{
+					PostMessage(WM_COMMAND, IDOK);
+					return TRUE;
+				}
+			}
+			break;
+		case VK_F5:
+			{
+				if (!m_bThreadRunning)
+				{
+					if(AfxBeginThread(PatchThreadEntry, this) == NULL)
+					{
+						CMessageBox::Show(this->m_hWnd, IDS_ERR_THREADSTARTFAILED, IDS_APPNAME, MB_OK | MB_ICONERROR);
+					}
+					else
+						m_bThreadRunning = TRUE;
+				}
+			}
+			break;
+		}
+	}
+
+	return CResizableStandAloneDialog::PreTranslateMessage(pMsg);
+}
+
+void CCreatePatch::OnBnClickedSelectall()
+{
+	UINT state = (m_SelectAll.GetState() & 0x0003);
+	if (state == BST_INDETERMINATE)
+	{
+		// It is not at all useful to manually place the checkbox into the indeterminate state...
+		// We will force this on to the unchecked state
+		state = BST_UNCHECKED;
+		m_SelectAll.SetCheck(state);
+	}
+	theApp.DoWaitCursor(1);
+	m_PatchList.SelectAll(state == BST_CHECKED);
+	theApp.DoWaitCursor(-1);
+}
+
+void CCreatePatch::OnBnClickedHelp()
+{
+	OnHelp();
+}
+
+void CCreatePatch::OnCancel()
+{
+	if (m_bThreadRunning)
+		return;
+
+	CResizableStandAloneDialog::OnCancel();
+}
+
+void CCreatePatch::OnOK()
+{
+	if (m_bThreadRunning)
+		return;
+
+	int nAddedFolders = 0;
+	int nListItems = m_PatchList.GetItemCount();
+	m_filesToRevert.Clear();
+	
+	for (int j=0; j<nListItems; j++)
+	{
+		const CSVNStatusListCtrl::FileEntry * entry = m_PatchList.GetListEntry(j);
+		if (entry->IsChecked())
+		{
+			if (entry->status == svn_wc_status_added)
+			{
+				if (entry->IsFolder())
+					nAddedFolders++;
+				else
+				{
+					// an added file. Is it inside an added folder?
+					for (int i=0; i<nListItems; ++i)
+					{
+						const CSVNStatusListCtrl::FileEntry * parententry = m_PatchList.GetListEntry(i);
+						if (parententry->status == svn_wc_status_added)
+						{
+							CTSVNPath checkpath = entry->GetPath().GetContainingDirectory();
+							while (!checkpath.IsEmpty())
+							{
+								if (checkpath.IsEquivalentTo(parententry->GetPath()))
+									nAddedFolders++;
+								checkpath = checkpath.GetContainingDirectory();
+							}
+						}
+					}
+				}	
+			}
+			// Unversioned files are not included in the resulting patchfile!
+			// We add those files to a list which will be used to add those files
+			// before creating the patch.
+			if ((entry->status == svn_wc_status_none)||(entry->status == svn_wc_status_unversioned))
+			{
+				m_filesToRevert.AddPath(entry->GetPath());
+			}
+		}
+	}
+
+	if (nAddedFolders != 0)
+	{
+		if (CMessageBox::Show(m_hWnd, IDS_CREATEPATCH_ADDEDFOLDERS, IDS_APPNAME, MB_YESNO | MB_ICONQUESTION)!=IDYES)
+			return;
+	}
+
+	if (m_filesToRevert.GetCount())
+	{
+		// add all unversioned files to version control
+		// so they're included in the resulting patch
+		// NOTE: these files must be reverted after the patch
+		// has been created! Since this dialog doesn't create the patch
+		// itself, the calling function is responsible to revert these files!
+		SVN svn;
+		svn.Add(m_filesToRevert, false);
+	}
+	
+	//save only the files the user has selected into the pathlist
+	m_PatchList.WriteCheckedNamesToPathList(m_pathList);
+
+	CResizableStandAloneDialog::OnOK();
+}
---
+++ tags/version_1.3.0RC1/src/TortoiseSVNSetup/CustomActions/CustomActions.cpp
@@ -0,0 +1,124 @@
+// CustomActions.cpp : Defines the entry point for the DLL application.
+//
+
+
+/* BIG FAT WARNING: Do not use any functions which require the C-Runtime library
+   in this custom action dll! The runtimes might not be installed yet!
+*/
+
+#include "stdafx.h"
+#include "shlwapi.h"
+#pragma comment(lib, "shlwapi")
+
+BOOL APIENTRY DllMain( HANDLE hModule, 
+                       DWORD  ul_reason_for_call, 
+                       LPVOID lpReserved
+					 )
+{
+    return TRUE;
+}
+
+UINT __stdcall TerminateCache(MSIHANDLE hModule)
+{
+	HWND hWnd = FindWindow(_T("TSVNCacheWindow"), _T("TSVNCacheWindow"));
+	if (hWnd)
+	{
+		// First, delete the registry key telling the shell where to find
+		// the cache. This is to make sure that the cache won't be started
+		// again right after we close it.
+		HKEY hKey = 0;
+		if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, _T("Software\\TortoiseSVN"), 0, KEY_WRITE, &hKey)==ERROR_SUCCESS)
+		{
+			RegDeleteValue(hKey, _T("CachePath"));
+			RegCloseKey(hKey);
+		}
+		PostMessage(hWnd, WM_CLOSE, NULL, NULL);
+		for (int i=0; i<10; ++i)
+		{
+			Sleep(500);
+			if (!IsWindow(hWnd))
+			{
+				// Cache is gone!
+				return ERROR_SUCCESS;
+			}
+		}
+		// Don't return ERROR_FUNCTION_FAILED, because even if the cache is still
+		// running, the installer will overwrite the file, and we require a
+		// reboot anyway after upgrading.
+		return ERROR_SUCCESS;
+	}
+	// cache wasn't even running
+	return ERROR_SUCCESS;
+}
+
+UINT __stdcall ClearRegistry(MSIHANDLE hModule)
+{
+	TCHAR entries[37][255] = {	_T("Software\\Classes\\Drive\\shellex\\ContextMenuHandlers\\TortoiseSVN"),
+		_T("Software\\Classes\\Directory\\shellex\\ContextMenuHandlers\\TortoiseSVN"),
+		_T("Software\\Classes\\Directory\\Background\\shellex\\ContextMenuHandlers\\TortoiseSVN"),
+		_T("Software\\Classes\\Folder\\shellex\\ContextMenuHandlers\\TortoiseSVN"),
+		_T("Software\\Classes\\lnkfile\\shellex\\ContextMenuHandlers\\TortoiseSVN"),
+		_T("Software\\Classes\\InternetShortcut\\shellex\\ContextMenuHandlers\\TortoiseSVN"),
+		_T("Software\\Classes\\*\\shellex\\ContextMenuHandlers\\TortoiseSVN"),
+
+		_T("Software\\Classes\\Drive\\shellex\\PropertySheetHandlers\\TortoiseSVN"),
+		_T("Software\\Classes\\Directory\\shellex\\PropertySheetHandlers\\TortoiseSVN"),
+		_T("Software\\Classes\\*\\shellex\\PropertySheetHandlers\\TortoiseSVN"),
+
+		_T("Software\\Classes\\Directory\\shellex\\DragDropHandlers\\TortoiseSVN"),
+		_T("Software\\Classes\\Folder\\shellex\\DragDropHandlers\\TortoiseSVN"),
+
+		_T("Software\\Classes\\Folder\\shellex\\ColumnHandlers\\{30351349-7B7D-4FCC-81B4-1E394CA267EB}"),
+
+		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\2TortoiseSVN"),
+
+		_T("Software\\Classes\\CLSID\\{30351349-7B7D-4FCC-81B4-1E394CA267EB}"),
+		_T("Software\\Classes\\CLSID\\{3035134A-7B7D-4FCC-81B4-1E394CA267EB}"),
+		_T("Software\\Classes\\CLSID\\{3035134E-7B7D-4FCC-81B4-1E394CA267EB}"),
+		_T("Software\\Classes\\CLSID\\{3035134D-7B7D-4FCC-81B4-1E394CA267EB}"),
+		_T("Software\\Classes\\CLSID\\{3035134C-7B7D-4FCC-81B4-1E394CA267EB}"),
+		_T("Software\\Classes\\CLSID\\{30351346-7B7D-4FCC-81B4-1E394CA267EB}"),
+		_T("Software\\Classes\\CLSID\\{3035134B-7B7D-4FCC-81B4-1E394CA267EB}"),
+		_T("Software\\Classes\\CLSID\\{30351347-7B7D-4FCC-81B4-1E394CA267EB}"),
+		_T("Software\\Classes\\CLSID\\{30351348-7B7D-4FCC-81B4-1E394CA267EB}"),
+
+		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\1TortoiseSVN"),
+		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\2TortoiseSVN"),
+		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\3TortoiseSVN"),
+		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\4TortoiseSVN"),
+		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\5TortoiseSVN"),
+		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\6TortoiseSVN"),
+		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\7TortoiseSVN"),
+		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\TortoiseSVN1"),
+		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\TortoiseSVN2"),
+		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\TortoiseSVN3"),
+		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\TortoiseSVN4"),
+		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\TortoiseSVN5"),
+		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\TortoiseSVN6"),
+		_T("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\ShellIconOverlayIdentifiers\\TortoiseSVN7")
+	};
+
+	for (int i=0; i<37; ++i)
+	{
+		HKEY hKey;
+		if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, entries[i], 0, KEY_WRITE, &hKey)==ERROR_SUCCESS)
+		{
+			SHDeleteKey(HKEY_LOCAL_MACHINE, (LPCTSTR)entries[i]);
+		}
+	}
+	for (int i=0; i<37; ++i)
+	{
+		HKEY hKey;
+		if (RegOpenKeyEx(HKEY_CURRENT_USER, entries[i], 0, KEY_WRITE, &hKey)==ERROR_SUCCESS)
+		{
+			SHDeleteKey(HKEY_CURRENT_USER, (LPCTSTR)entries[i]);
+		}
+	}
+	return ERROR_SUCCESS;
+}
+
+UINT __stdcall MsgBox(MSIHANDLE hModule)
+{
+	MessageBox(NULL, _T("CustomAction \"MsgBox\" running"), _T("Installer"), MB_ICONINFORMATION);
+	return ERROR_SUCCESS;
+}
