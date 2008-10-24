b77b32a6f4d60beaaf7f0e82062d925784ee2b2c
tortoisesvn <tortoisesvn@4cfb86a4-2c7a-11df-bd14-c1e331e6ceac>
2008-10-24 16:07:19 +0000

    Tag the 1.5.5 release

---
+++ tags/version-1.5.5/contrib/issue-tracker-plugins/ExampleCsPlugin/MyPlugin.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace ExampleCsPlugin
+{
+    [ComVisible(true),
+        Guid("5870B3F1-8393-4c83-ACED-1D5E803A4F2B"),
+        ClassInterface(ClassInterfaceType.None)]
+    public class MyPlugin : Interop.BugTraqProvider.IBugTraqProvider
+    {
+        public bool ValidateParameters(IntPtr hParentWnd, string parameters)
+        {
+            return true;
+        }
+
+        public string GetLinkText(IntPtr hParentWnd, string parameters)
+        {
+            return "Choose Issue";
+        }
+
+        public string GetCommitMessage(IntPtr hParentWnd, string parameters, string commonRoot, string[] pathList,
+                                       string originalMessage)
+        {
+            try
+            {
+                List<TicketItem> tickets = new List<TicketItem>();
+                tickets.Add(new TicketItem(12, "Service doesn't start on Windows Vista"));
+                tickets.Add(new TicketItem(19, "About box doesn't render correctly in large fonts mode"));
+
+/*
+                tickets.Add(new TicketItem(88, commonRoot));
+                foreach (string path in pathList)
+                    tickets.Add(new TicketItem(99, path));
+ */
+
+                MyIssuesForm form = new MyIssuesForm(tickets);
+                if (form.ShowDialog() != DialogResult.OK)
+                    return originalMessage;
+
+                StringBuilder result = new StringBuilder(originalMessage);
+                if (originalMessage.Length != 0 && !originalMessage.EndsWith("\n"))
+                    result.AppendLine();
+
+                foreach (TicketItem ticket in form.TicketsFixed)
+                {
+                    result.AppendFormat("Fixed #{0}: {1}", ticket.Number, ticket.Summary);
+                    result.AppendLine();
+                }
+
+                return result.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());                
+                throw;
+            }
+        }
+    }
+}
---
+++ tags/version-1.5.5/src/SVN/SVNRev.h
@@ -0,0 +1,163 @@
+// TortoiseSVN - a Windows shell extension for easy version control
+
+// Copyright (C) 2003-2008 - TortoiseSVN
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#pragma once
+#include "svn_opt.h"
+#include <vector>
+
+
+/**
+ * \ingroup SVN
+ * SVNRev represents a subversion revision. A subversion revision can
+ * be either a simple revision number, a date or a string with one
+ * of the following keywords:
+ * - BASE
+ * - COMMITTED
+ * - PREV
+ * - HEAD
+ *
+ * For convenience, this class also accepts the string "WC" as a
+ * keyword for the working copy revision.
+ */
+class SVNRev
+{
+public:
+	SVNRev(LONG nRev);
+	SVNRev(CString sRev);
+	SVNRev(svn_opt_revision_t revision) {rev = revision;}
+	SVNRev(){rev.kind = svn_opt_revision_unspecified;m_bIsValid = FALSE;}
+	~SVNRev();
+
+	/// returns TRUE if the revision is valid (i.e. not unspecified)
+	BOOL IsValid() const {return m_bIsValid;}
+	/// returns TRUE if the revision is HEAD
+	BOOL IsHead() const {return (rev.kind == svn_opt_revision_head);}
+	/// returns TRUE if the revision is BASE
+	BOOL IsBase() const {return (rev.kind == svn_opt_revision_base);}
+	/// returns TRUE if the revision is WORKING
+	BOOL IsWorking() const {return (rev.kind == svn_opt_revision_working);}
+	/// returns TRUE if the revision is PREV
+	BOOL IsPrev() const {return (rev.kind == svn_opt_revision_previous);}
+	/// returns TRUE if the revision is COMMITTED
+	BOOL IsCommitted() const {return (rev.kind == svn_opt_revision_committed);}
+	/// returns TRUE if the revision is a date
+	BOOL IsDate() const {return (rev.kind == svn_opt_revision_date);}
+	/// returns TRUE if the revision is a number
+	BOOL IsNumber() const {return (rev.kind == svn_opt_revision_number);}
+
+	// Returns the kind of revision representation (number, HEAD, BASE, etc.)
+	svn_opt_revision_kind GetKind() const {return rev.kind;}
+
+	/// Returns a string representing the date of a DATE revision, otherwise an empty string.
+	CString GetDateString() const {return sDate;}
+	/// Returns the date if the revision is of type svn_opt_revision_date
+	apr_time_t GetDate() const {ATLASSERT(IsDate()); return rev.value.date;}
+	/// Converts the revision into a string representation.
+	CString ToString() const;
+	/// checks whether two SVNRev objects are the same
+	bool IsEqual(const SVNRev& revision);
+	
+	operator LONG () const;
+	operator svn_opt_revision_t * ();
+	operator const svn_opt_revision_t * () const;
+	enum
+	{
+		REV_HEAD = -1,			///< head revision
+		REV_BASE = -2,			///< base revision
+		REV_WC = -3,			///< revision of the working copy
+		REV_UNSPECIFIED = -4,	///< unspecified revision
+	};
+protected:
+	void Create(svn_revnum_t nRev);
+	void Create(CString sRev);
+private:
+	svn_opt_revision_t rev;
+	BOOL m_bIsValid;
+	CString sDate;
+};
+
+/**
+ * \ingroup SVN
+ * represents a revision range.
+ */
+class SVNRevRange
+{
+public:
+	SVNRevRange()
+	{
+		SecureZeroMemory(&revrange, sizeof(svn_opt_revision_range_t));
+	}
+	SVNRevRange(const SVNRev& start, const SVNRev& end) 
+	{
+		SecureZeroMemory(&revrange, sizeof(svn_opt_revision_range_t));
+		revrange.start = *(const svn_opt_revision_t*)start;
+		revrange.end = *(const svn_opt_revision_t*)end;
+	}
+
+	void		SetRange(const SVNRev& rev1, const SVNRev& rev2) {revrange.start = *(const svn_opt_revision_t*)rev1; revrange.end = *(const svn_opt_revision_t*)rev2;}
+	SVNRev		GetStartRevision() {return SVNRev(revrange.start);}
+	SVNRev		GetEndRevision() {return SVNRev(revrange.end);}
+
+	operator svn_opt_revision_range_t * () {return &revrange;}
+
+private:
+	svn_opt_revision_range_t revrange;
+};
+
+
+class SVNRevRangeArray
+{
+public:
+	SVNRevRangeArray() {}
+	~SVNRevRangeArray() {}
+
+	int					AddRevision(const SVNRev& revision);
+	int					AddRevRange(const SVNRevRange& revrange);
+	int					AddRevRange(const SVNRev& start, const SVNRev& end);
+	int					GetCount() const;
+	void				Clear();
+	void				AdjustForMerge(bool bReverse = false);
+
+	const apr_array_header_t* GetAprArray(apr_pool_t * pool);
+
+	const SVNRevRange&	operator[](int index) const;
+
+	bool				FromListString(const CString& string);
+	CString				ToListString(bool bReverse = false);
+
+	struct AscSort
+	{
+		bool operator()(SVNRevRange& pStart, SVNRevRange& pEnd)
+		{
+			return svn_revnum_t(pStart.GetStartRevision()) < svn_revnum_t(pEnd.GetStartRevision());
+		}
+	};
+
+	struct DescSort
+	{
+		bool operator()(SVNRevRange& pStart, SVNRevRange& pEnd)
+		{
+			return svn_revnum_t(pStart.GetStartRevision()) > svn_revnum_t(pEnd.GetStartRevision());
+		}
+	};
+
+private:
+	std::vector<SVNRevRange>	m_array;
+};
+
