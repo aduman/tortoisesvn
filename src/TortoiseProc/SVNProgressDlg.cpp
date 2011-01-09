aduman/tortoisesvn

commit 258f322176b1e4cf6c3d74bbb5b23f9b8daf9bd3

Start the log dialog in a separate process instead of modal.
git-svn-id: https://svn.osdn.net/svnroot/tortoisesvn/trunk@20635 154be670-7969-47af-9ab0-c6fb378d2295

src/TortoiseProc/SVNProgressDlg.cpp
@@ -1185,9 +1185,11 @@ void CSVNProgressDlg::OnBnClickedLogbutton()
     {
         rev = it->second;
     }
-    CLogDlg dlg;
-    dlg.SetParams(m_targetPathList[0], m_RevisionEnd, m_RevisionEnd, rev, 0, TRUE);
-    dlg.DoModal();
+    CString sCmd;
+    sCmd.Format(_T("\"%s\" /command:log /path:\"%s\" /startrev:%s"),
+        (LPCTSTR)(CPathUtils::GetAppDirectory()+_T("TortoiseProc.exe")), m_targetPathList[0].GetWinPath(), (LPCTSTR)m_RevisionEnd.ToString());
+
+    CAppUtils::LaunchApplication(sCmd, NULL, false);
 }
 
 void CSVNProgressDlg::OnBnClickedRetrynohooks()
@@ -1949,20 +1951,15 @@ void CSVNProgressDlg::OnContextMenu(CWnd* pWnd, CPoint point)
         break;
     case ID_LOG:
         {
-            svn_revnum_t rev = m_RevisionEnd;
-            if (!data->basepath.IsEmpty())
-            {
-                StringRevMap::iterator it = m_FinishedRevMap.find(data->basepath.GetSVNApiPath(pool));
-                if (it != m_FinishedRevMap.end())
-                    rev = it->second;
-            }
-            CLogDlg dlg;
             // fetch the log from HEAD, not the revision we updated to:
             // the path might be inside an external folder which has its own
             // revisions.
             CString sPath = GetPathFromColumnText(data->sPathColumnText);
-            dlg.SetParams(CTSVNPath(sPath), SVNRev(), SVNRev::REV_HEAD, 1, TRUE);
-            dlg.DoModal();
+            CString sCmd;
+            sCmd.Format(_T("\"%s\" /command:log /path:\"%s\""),
+                (LPCTSTR)(CPathUtils::GetAppDirectory()+_T("TortoiseProc.exe")), (LPCTSTR)sPath);
+
+            CAppUtils::LaunchApplication(sCmd, NULL, false);
         }
         break;
     case ID_OPENWITH:

commit 138f1a09b3ff33cb17665212dfb26ad1ff84584f

Patch from Daniel Klima: finally fix bug introduced in r20635.

src/TortoiseProc/SVNProgressDlg.cpp
@@ -1185,10 +1185,10 @@ void CSVNProgressDlg::OnBnClickedLogbutton()
     {
         rev = it->second;
     }
-    SVNRev startRev(rev);
+    SVNRev endRev(rev);
     CString sCmd;
-    sCmd.Format(_T("\"%s\" /command:log /path:\"%s\" /startrev:%s"),
-        (LPCTSTR)(CPathUtils::GetAppDirectory()+_T("TortoiseProc.exe")), m_targetPathList[0].GetWinPath(), (LPCTSTR)startRev.ToString());
+    sCmd.Format(_T("\"%s\" /command:log /path:\"%s\" /endrev:%s"),
+        (LPCTSTR)(CPathUtils::GetAppDirectory()+_T("TortoiseProc.exe")), m_targetPathList[0].GetWinPath(), (LPCTSTR)endRev.ToString());
 
     CAppUtils::LaunchApplication(sCmd, NULL, false);
 }