aduman/tortoisesvn

commit 3ba294db563696c8d35094faa4d791a432c5b7d2

* show a custom cursor if the mousepointer is over the margin * a click on the margin selects the corresponding line
git-svn-id: https://svn.code.sf.net/p/tortoisesvn/code/trunk@21100 154be670-7969-47af-9ab0-c6fb378d2295

src/TortoiseMerge/BaseView.cpp
@@ -83,6 +83,7 @@ CBaseView::CBaseView()
     m_nOffsetChar = 0;
     m_nDigits = 0;
     m_nMouseLine = -1;
+    m_mouseInMargin = false;
     m_bMouseWithin = FALSE;
     m_bIsHidden = FALSE;
     lineendings = EOL_AUTOLINE;
@@ -123,6 +124,7 @@ CBaseView::CBaseView()
     m_hLineEndingLF = LoadIcon(IDI_LINEENDINGLF);
     m_hEditedIcon = LoadIcon(IDI_LINEEDITED);
     m_hMovedIcon = LoadIcon(IDI_MOVEDLINE);
+    m_margincursor = (HCURSOR)LoadImage(AfxGetResourceHandle(), MAKEINTRESOURCE(IDC_MARGINCURSOR), IMAGE_CURSOR, 0, 0, LR_DEFAULTSIZE);
 
     m_nCachedWrappedLine = -1;
 
@@ -147,6 +149,7 @@ CBaseView::~CBaseView()
     DestroyIcon(m_hLineEndingLF);
     DestroyIcon(m_hEditedIcon);
     DestroyIcon(m_hMovedIcon);
+    DestroyCursor(m_margincursor);
 }
 
 BEGIN_MESSAGE_MAP(CBaseView, CView)
@@ -1953,6 +1956,11 @@ BOOL CBaseView::OnSetCursor(CWnd* pWnd, UINT nHitTest, UINT message)
                 }
             }
         }
+        if (m_mouseInMargin)
+        {
+            ::SetCursor(m_margincursor);
+            return TRUE;
+        }
         ::SetCursor(::LoadCursor(NULL, MAKEINTRESOURCE(IDC_ARROW)));    // Set To Arrow Cursor
         return TRUE;
     }
@@ -2485,6 +2493,12 @@ void CBaseView::OnLButtonDown(UINT nFlags, CPoint point)
         {
             ClearSelection();
             SetupSelection(m_ptCaretPos.y, m_ptCaretPos.y);
+            if (point.x < GetMarginWidth())
+            {
+                // select the whole line
+                m_ptSelectionStartPos = m_ptSelectionEndPos = m_ptCaretPos;
+                m_ptSelectionEndPos.x = GetLineLength(m_ptSelectionEndPos.y);
+            }
         }
 
         UpdateViewsCaretPosition();
@@ -2678,16 +2692,17 @@ void CBaseView::OnEditCopy()
 
 void CBaseView::OnMouseMove(UINT nFlags, CPoint point)
 {
-    if (m_pMainFrame->m_nMoveMovesToIgnore > 0) {
+    if (m_pMainFrame->m_nMoveMovesToIgnore > 0)
+    {
         --m_pMainFrame->m_nMoveMovesToIgnore;
         CView::OnMouseMove(nFlags, point);
         return;
     }
     int nMouseLine = GetButtonEventLineIndex(point);
     if (nMouseLine < -1)
-    {
         nMouseLine = -1;
-    }
+    m_mouseInMargin = point.x < GetMarginWidth();
+
     ShowDiffLines(nMouseLine);
 
     KillTimer(IDT_SCROLLTIMER);
@@ -3581,6 +3596,7 @@ void CBaseView::AdjustSelection()
     {
         m_ptSelectionStartPos = m_ptSelectionOrigin;
         m_ptSelectionEndPos = m_ptCaretPos;
+        m_ptSelectionEndPos.x = GetLineLength(m_ptCaretPos.y);
     }
 
     SetupSelection(min(m_ptSelectionStartPos.y, m_ptSelectionEndPos.y), max(m_ptSelectionStartPos.y, m_ptSelectionEndPos.y));

src/TortoiseMerge/BaseView.h
@@ -299,6 +299,8 @@ protected:
     int             m_nSelBlockEnd;
 
     int             m_nMouseLine;
+    bool            m_mouseInMargin;
+    HCURSOR         m_margincursor;
 
     bool            m_bCaretHidden;
     POINT           m_ptCaretPos;

src/TortoiseMerge/TortoiseMerge.vcxproj
@@ -606,6 +606,7 @@
     <None Include="..\Resources\lineequal.ico" />
     <None Include="..\Resources\lineremoved.ico" />
     <None Include="..\Resources\linewhitespace.ico" />
+    <None Include="..\Resources\Margin.cur" />
     <None Include="..\Resources\moved.ico" />
     <None Include="..\Resources\Toolbar.bmp" />
     <None Include="..\Resources\TortoiseMerge.ico" />
@@ -613,6 +614,7 @@
     <None Include="..\Resources\tortoisemerge_logoflipped.bmp" />
     <None Include="..\Resources\tortoisesvn_logoflipped.bmp" />
     <None Include="..\Resources\ttf_glyph.bmp" />
+    <None Include="Margin.cur" />
   </ItemGroup>
   <ItemGroup>
     <ResourceCompile Include="..\Resources\TortoiseMergeENG.rc" />

src/TortoiseMerge/TortoiseMerge.vcxproj.filters
@@ -442,6 +442,12 @@
     <None Include="..\Resources\moved.ico">
       <Filter>Resource Files</Filter>
     </None>
+    <None Include="Margin.cur">
+      <Filter>Resource Files</Filter>
+    </None>
+    <None Include="..\Resources\Margin.cur">
+      <Filter>Resource Files</Filter>
+    </None>
   </ItemGroup>
   <ItemGroup>
     <ResourceCompile Include="..\Resources\TortoiseMergeENG.rc">

commit 0082ec3e17a592abaa9150aabdab18a85097c4c0

Fix bug introduced in r21100: Only select up to the cursor, not the whole line.

src/TortoiseMerge/BaseView.cpp
@@ -3623,7 +3623,6 @@ void CBaseView::AdjustSelection()
     {
         m_ptSelectionStartPos = m_ptSelectionOrigin;
         m_ptSelectionEndPos = m_ptCaretPos;
-        m_ptSelectionEndPos.x = GetLineLength(m_ptCaretPos.y);
     }
 
     SetupSelection(min(m_ptSelectionStartPos.y, m_ptSelectionEndPos.y), max(m_ptSelectionStartPos.y, m_ptSelectionEndPos.y));