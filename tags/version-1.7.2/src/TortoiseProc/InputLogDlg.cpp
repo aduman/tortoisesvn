4cba7816ee9713af4dc59ca112075b01869e86f9
tortoisesvn <tortoisesvn@4cfb86a4-2c7a-11df-bd14-c1e331e6ceac>
2011-12-05 17:35:11 +0000

    Create the 1.7.2 tag

---
+++ tags/version-1.7.2/src/TortoiseProc/InputLogDlg.cpp
@@ -0,0 +1,214 @@
+// TortoiseSVN - a Windows shell extension for easy version control
+
+// Copyright (C) 2003-2011 - TortoiseSVN
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+#include "stdafx.h"
+#include "TortoiseProc.h"
+#include "InputLogDlg.h"
+#include "Registry.h"
+#include "HistoryDlg.h"
+#include "RegHistory.h"
+#include "AppUtils.h"
+
+
+// CInputLogDlg dialog
+
+IMPLEMENT_DYNAMIC(CInputLogDlg, CResizableStandAloneDialog)
+
+CInputLogDlg::CInputLogDlg(CWnd* pParent /*=NULL*/)
+    : CResizableStandAloneDialog(CInputLogDlg::IDD, pParent)
+    , m_pProjectProperties(NULL)
+    , m_bForceFocus(false)
+    , m_iCheck(0)
+{
+
+}
+
+CInputLogDlg::~CInputLogDlg()
+{
+}
+
+void CInputLogDlg::DoDataExchange(CDataExchange* pDX)
+{
+    CResizableStandAloneDialog::DoDataExchange(pDX);
+    DDX_Control(pDX, IDC_INPUTTEXT, m_cInput);
+    DDX_Check(pDX, IDC_CHECKBOX, m_iCheck);
+}
+
+
+BEGIN_MESSAGE_MAP(CInputLogDlg, CResizableStandAloneDialog)
+    ON_EN_CHANGE(IDC_INPUTTEXT, OnEnChangeLogmessage)
+    ON_BN_CLICKED(IDC_HISTORY, &CInputLogDlg::OnBnClickedHistory)
+END_MESSAGE_MAP()
+
+
+BOOL CInputLogDlg::OnInitDialog()
+{
+    CResizableStandAloneDialog::OnInitDialog();
+    CAppUtils::MarkWindowAsUnpinnable(m_hWnd);
+
+    ExtendFrameIntoClientArea(IDC_GROUPBOX);
+    m_aeroControls.SubclassControl(this, IDC_CHECKBOX);
+    m_aeroControls.SubclassOkCancel(this);
+
+#ifdef DEBUG
+    if (m_pProjectProperties == NULL)
+        TRACE("InputLogDlg: project properties not set\n");
+    if (m_sActionText.IsEmpty())
+        TRACE("InputLogDlg: action text not set\n");
+    if (m_sUUID.IsEmpty())
+        TRACE("InputLogDlg: repository UUID not set\n");
+#endif
+
+    if (m_pProjectProperties)
+        m_cInput.Init(*m_pProjectProperties);
+    else
+        m_cInput.Init();
+
+    m_cInput.SetFont((CString)CRegString(_T("Software\\TortoiseSVN\\LogFontName"), _T("Courier New")), (DWORD)CRegDWORD(_T("Software\\TortoiseSVN\\LogFontSize"), 8));
+
+    if (m_pProjectProperties)
+    {
+        if (m_pProjectProperties->nLogWidthMarker)
+        {
+            m_cInput.Call(SCI_SETWRAPMODE, SC_WRAP_NONE);
+            m_cInput.Call(SCI_SETEDGEMODE, EDGE_LINE);
+            m_cInput.Call(SCI_SETEDGECOLUMN, m_pProjectProperties->nLogWidthMarker);
+        }
+        else
+        {
+            m_cInput.Call(SCI_SETEDGEMODE, EDGE_NONE);
+            m_cInput.Call(SCI_SETWRAPMODE, SC_WRAP_WORD);
+        }
+        m_cInput.SetText(m_pProjectProperties->GetLogMsgTemplate(m_sSVNAction));
+    }
+
+    UpdateOKButton();
+
+    CAppUtils::SetAccProperty(m_cInput.GetSafeHwnd(), PROPID_ACC_ROLE, ROLE_SYSTEM_TEXT);
+    CAppUtils::SetAccProperty(m_cInput.GetSafeHwnd(), PROPID_ACC_HELP, CString(MAKEINTRESOURCE(IDS_INPUT_ENTERLOG)));
+
+    SetDlgItemText(IDC_ACTIONLABEL, m_sActionText);
+    if (!m_sTitleText.IsEmpty())
+        SetWindowText(m_sTitleText);
+    if (!m_sCheckText.IsEmpty())
+    {
+        SetDlgItemText(IDC_CHECKBOX, m_sCheckText);
+        GetDlgItem(IDC_CHECKBOX)->ShowWindow(SW_SHOW);
+    }
+    else
+    {
+        GetDlgItem(IDC_CHECKBOX)->ShowWindow(SW_HIDE);
+    }
+
+    AddAnchor(IDC_ACTIONLABEL, TOP_LEFT, TOP_RIGHT);
+    AddAnchor(IDC_GROUPBOX, TOP_LEFT, BOTTOM_RIGHT);
+    AddAnchor(IDC_HISTORY, TOP_LEFT);
+    AddAnchor(IDC_INPUTTEXT, TOP_LEFT, BOTTOM_RIGHT);
+    AddAnchor(IDC_CHECKBOX, BOTTOM_LEFT, BOTTOM_RIGHT);
+    AddAnchor(IDCANCEL, BOTTOM_RIGHT);
+    AddAnchor(IDOK, BOTTOM_RIGHT);
+    EnableSaveRestore(_T("InputLogDlg"));
+    if (GetExplorerHWND())
+        CenterWindow(CWnd::FromHandle(GetExplorerHWND()));
+
+    // HACK! Under certain conditions, the dialog box
+    // will not get the input focus.
+    // Call SetForceFocus() to work around that problem.
+
+    if (m_bForceFocus)
+    {
+        ShowWindow (SW_HIDE);
+        ShowWindow (SW_SHOW);
+    }
+
+    GetDlgItem(IDC_INPUTTEXT)->SetFocus();
+    return FALSE;
+}
+
+void CInputLogDlg::OnOK()
+{
+    UpdateData();
+    m_sLogMsg = m_cInput.GetText();
+
+    CString reg;
+    reg.Format(_T("Software\\TortoiseSVN\\History\\commit%s"), (LPCTSTR)m_sUUID);
+
+    CRegHistory history;
+    history.Load(reg, _T("logmsgs"));
+    history.AddEntry(m_sLogMsg);
+    history.Save();
+
+    CResizableStandAloneDialog::OnOK();
+}
+
+BOOL CInputLogDlg::PreTranslateMessage(MSG* pMsg)
+{
+    if (pMsg->message == WM_KEYDOWN)
+    {
+        switch (pMsg->wParam)
+        {
+        case VK_RETURN:
+            if (OnEnterPressed())
+                return TRUE;
+            break;
+        }
+    }
+
+    return CResizableStandAloneDialog::PreTranslateMessage(pMsg);
+}
+
+void CInputLogDlg::OnEnChangeLogmessage()
+{
+    UpdateOKButton();
+}
+
+void CInputLogDlg::UpdateOKButton()
+{
+    CString sTemp = m_cInput.GetText();
+    if (((m_pProjectProperties==NULL)||(sTemp.GetLength() >= m_pProjectProperties->nMinLogSize)))
+    {
+        DialogEnableWindow(IDOK, TRUE);
+    }
+    else
+    {
+        DialogEnableWindow(IDOK, FALSE);
+    }
+}
+
+void CInputLogDlg::OnBnClickedHistory()
+{
+    CString reg;
+    reg.Format(_T("Software\\TortoiseSVN\\History\\commit%s"), (LPCTSTR)m_sUUID);
+    CRegHistory history;
+    history.Load(reg, _T("logmsgs"));
+    CHistoryDlg HistoryDlg;
+    HistoryDlg.SetHistory(history);
+    if (HistoryDlg.DoModal()==IDOK)
+    {
+        if (HistoryDlg.GetSelectedText().Compare(m_cInput.GetText().Left(HistoryDlg.GetSelectedText().GetLength()))!=0)
+        {
+            if ((m_pProjectProperties)&&(m_pProjectProperties->GetLogMsgTemplate(m_sSVNAction).Compare(m_cInput.GetText())!=0))
+                m_cInput.InsertText(HistoryDlg.GetSelectedText(), !m_cInput.GetText().IsEmpty());
+            else
+                m_cInput.SetText(HistoryDlg.GetSelectedText());
+        }
+
+        UpdateOKButton();
+        GetDlgItem(IDC_INPUTTEXT)->SetFocus();
+    }
+}
---
+++ tags/version-1.7.2/src/TortoiseProc/Properties/EditPropBase.cpp
@@ -0,0 +1,61 @@
+// TortoiseSVN - a Windows shell extension for easy version control
+
+// Copyright (C) 2010 - TortoiseSVN
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+#include "stdafx.h"
+#include "TortoiseProc.h"
+#include "SVNProperties.h"
+#include "UnicodeUtils.h"
+#include "AppUtils.h"
+#include "StringUtils.h"
+#include "EditPropBase.h"
+#include "auto_buffer.h"
+
+
+EditPropBase::EditPropBase()
+    : m_bFolder(false)
+    , m_bMultiple(false)
+    , m_bIsBinary(false)
+    , m_bChanged(false)
+    , m_bRevProps(false)
+    , m_bRecursive(false)
+{
+}
+
+EditPropBase::~EditPropBase()
+{
+}
+
+void EditPropBase::SetPropertyName(const std::string& sName)
+{
+    m_PropName = sName;
+}
+
+void EditPropBase::SetPropertyValue(const std::string& sValue)
+{
+    if (SVNProperties::IsBinary(sValue))
+    {
+        m_bIsBinary = true;
+    }
+    else
+    {
+        m_bIsBinary = false;
+    }
+    m_PropValue = sValue;
+}
+
+
