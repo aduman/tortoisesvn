fefb86aabdfe5eaeb31a984c1c37b5fafbe910e5
tortoisesvn <tortoisesvn@4cfb86a4-2c7a-11df-bd14-c1e331e6ceac>
2014-08-09 08:24:29 +0000

    Tag version 1.7.15

---
+++ tags/version-1.7.15/src/SVN/SVNConfig.cpp
@@ -0,0 +1,263 @@
+// TortoiseSVN - a Windows shell extension for easy version control
+
+// Copyright (C) 2003-2007, 2010, 2012 - TortoiseSVN
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#include "StdAfx.h"
+#include "SVNConfig.h"
+#include "UnicodeUtils.h"
+#include "PathUtils.h"
+#include "SVNGlobal.h"
+#pragma warning(push)
+#include "svn_error.h"
+#include "svn_pools.h"
+#include "svn_config.h"
+#include "svn_wc.h"
+#pragma warning(pop)
+
+SVNConfig* SVNConfig::m_pInstance;
+
+
+SVNConfig::SVNConfig(void)
+    : config(nullptr)
+    , patterns(nullptr)
+{
+    m_critSec.Init();
+    parentpool = svn_pool_create(NULL);
+
+    svn_error_t * err = svn_config_ensure(NULL, parentpool);
+    pool = svn_pool_create (parentpool);
+    // set up the configuration
+    if (err == 0)
+        err = svn_config_get_config (&(config), g_pConfigDir, parentpool);
+
+    if (err != 0)
+    {
+        svn_error_clear(err);
+    }
+}
+
+SVNConfig::~SVNConfig(void)
+{
+    svn_pool_destroy (pool);
+    svn_pool_destroy (parentpool);
+    m_critSec.Term();
+    delete m_pInstance;
+}
+
+BOOL SVNConfig::GetDefaultIgnores()
+{
+    if (config == nullptr)
+        return FALSE;
+    svn_error_t * err;
+    patterns = NULL;
+    err = svn_wc_get_default_ignores (&(patterns), config, pool);
+    if (err)
+    {
+        svn_error_clear(err);
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+BOOL SVNConfig::MatchIgnorePattern(const CString& name)
+{
+    if (patterns == NULL)
+        return FALSE;
+    return svn_wc_match_ignore_list(CUnicodeUtils::GetUTF8(name), patterns, pool);
+}
+
+BOOL SVNConfig::KeepLocks()
+{
+    if (config == nullptr)
+        return FALSE;
+    svn_boolean_t no_unlock = FALSE;
+    svn_config_t * opt = (svn_config_t *)apr_hash_get (config, SVN_CONFIG_CATEGORY_CONFIG,
+        APR_HASH_KEY_STRING);
+    if (opt)
+        svn_error_clear(svn_config_get_bool(opt, &no_unlock, SVN_CONFIG_SECTION_MISCELLANY, SVN_CONFIG_OPTION_NO_UNLOCK, FALSE));
+    return no_unlock;
+}
+
+bool SVNConfig::SetUpSSH(svn_client_ctx_t * ctx)
+{
+    bool bRet = false;
+    //set up the SVN_SSH param
+    CString tsvn_ssh = CRegString(_T("Software\\TortoiseSVN\\SSH"));
+    if (tsvn_ssh.IsEmpty() && ctx->config)
+    {
+        // check whether the ssh client is already set in the Subversion config
+        svn_config_t * cfg = (svn_config_t *)apr_hash_get (ctx->config, SVN_CONFIG_CATEGORY_CONFIG,
+            APR_HASH_KEY_STRING);
+        if (cfg)
+        {
+            const char * sshValue = NULL;
+            svn_config_get(cfg, &sshValue, SVN_CONFIG_SECTION_TUNNELS, "ssh", "");
+            if ((sshValue == NULL)||(sshValue[0] == 0))
+                tsvn_ssh = _T("\"") + CPathUtils::GetAppDirectory() + _T("TortoisePlink.exe") + _T("\"");
+        }
+    }
+    tsvn_ssh.Replace('\\', '/');
+    if (!tsvn_ssh.IsEmpty() && ctx->config)
+    {
+        svn_config_t * cfg = (svn_config_t *)apr_hash_get (ctx->config, SVN_CONFIG_CATEGORY_CONFIG,
+            APR_HASH_KEY_STRING);
+        if (cfg)
+        {
+            svn_config_set(cfg, SVN_CONFIG_SECTION_TUNNELS, "ssh", CUnicodeUtils::GetUTF8(tsvn_ssh));
+            bRet = true;
+        }
+    }
+    return bRet;
+}
+
+
+// TODO: remove these forward declarations
+// once svn 1.8 is out and the config copy
+// APIs can be used.
+struct svn_config_t
+{
+  /* Table of cfg_section_t's. */
+  apr_hash_t *sections;
+
+  /* Pool for hash tables, table entries and unexpanded values */
+  apr_pool_t *pool;
+
+  /* Pool for expanded values -- this is separate, so that we can
+     clear it when modifying the config data. */
+  apr_pool_t *x_pool;
+
+  /* Indicates that some values in the configuration have been expanded. */
+  svn_boolean_t x_values;
+
+  /* Temporary string used for lookups.  (Using a stringbuf so that
+     frequent resetting is efficient.) */
+  svn_stringbuf_t *tmp_key;
+
+  /* Temporary value used for expanded default values in svn_config_get.
+     (Using a stringbuf so that frequent resetting is efficient.) */
+  svn_stringbuf_t *tmp_value;
+
+  /* Specifies whether section names are populated case sensitively. */
+  svn_boolean_t section_names_case_sensitive;
+};
+
+/* Section table entries. */
+typedef struct cfg_section_t cfg_section_t;
+struct cfg_section_t
+{
+  /* The section name. */
+  const char *name;
+
+  /* The section name, converted into a hash key. */
+  const char *hash_key;
+
+  /* Table of cfg_option_t's. */
+  apr_hash_t *options;
+};
+
+
+/* Option table entries. */
+typedef struct cfg_option_t cfg_option_t;
+struct cfg_option_t
+{
+  /* The option name. */
+  const char *name;
+
+  /* The option name, converted into a hash key. */
+  const char *hash_key;
+
+  /* The unexpanded option value. */
+  const char *value;
+
+  /* The expanded option value. */
+  const char *x_value;
+
+  /* Expansion flag. If this is TRUE, this value has already been expanded.
+     In this case, if x_value is NULL, no expansions were necessary,
+     and value should be used directly. */
+  svn_boolean_t expanded;
+};
+
+apr_hash_t * SVNConfig::GetConfig( apr_pool_t * pool )
+{
+    if (config == NULL)
+        return NULL;
+    // create a copy of the config object we have
+    // to avoid threading issues
+    CComCritSecLock<CComCriticalSection> lock(m_critSec);
+
+    // TODO: once 1.8 is out, use svn_config_copy_config() instead
+    // of this custom copy stuff.
+    apr_hash_t * rethash = apr_hash_make(pool);
+    for (apr_hash_index_t * cidx = apr_hash_first(pool, config); cidx != NULL; cidx = apr_hash_next(cidx))
+    {
+        const void *ckey = NULL;
+        void *cval = NULL;
+        apr_ssize_t ckeyLength = 0;
+
+        apr_hash_this(cidx, &ckey, &ckeyLength, &cval);
+        svn_config_t * c = (svn_config_t*)cval;
+
+
+        svn_config_t * newconfig = NULL;
+        svn_config_create(&newconfig, false, pool);
+
+        newconfig->sections = apr_hash_make(pool);
+        newconfig->pool = pool;
+        newconfig->x_pool = svn_pool_create(pool);
+        newconfig->x_values = c->x_values;
+        newconfig->tmp_key = svn_stringbuf_dup(c->tmp_key, pool);
+        newconfig->tmp_value = svn_stringbuf_dup(c->tmp_value, pool);
+        newconfig->section_names_case_sensitive = c->section_names_case_sensitive;
+
+        for (apr_hash_index_t * sectidx = apr_hash_first(pool, c->sections); sectidx != NULL; sectidx = apr_hash_next(sectidx))
+        {
+            const void *sectkey = NULL;
+            void *sectval = NULL;
+            apr_ssize_t sectkeyLength = 0;
+            apr_hash_this(sectidx, &sectkey, &sectkeyLength, &sectval);
+            cfg_section_t * sect = (cfg_section_t*)sectval;
+
+            cfg_section_t * newsec = (cfg_section_t *)apr_palloc(pool, sizeof(*newsec));
+            newsec->name = apr_pstrdup(pool, sect->name);
+            newsec->hash_key = apr_pstrdup(pool, sect->hash_key);
+            newsec->options = apr_hash_make(pool);
+            for (apr_hash_index_t * optidx = apr_hash_first(pool, sect->options); optidx != NULL; optidx = apr_hash_next(optidx))
+            {
+                const void *optkey = NULL;
+                void *optval = NULL;
+                apr_ssize_t optkeyLength = 0;
+                apr_hash_this(optidx, &optkey, &optkeyLength, &optval);
+                cfg_option_t * opt = (cfg_option_t*)optval;
+
+                cfg_option_t * newopt = (cfg_option_t*)apr_palloc(pool, sizeof(*newopt));
+                newopt->name = apr_pstrdup(pool, opt->name);
+                newopt->hash_key = apr_pstrdup(pool, opt->hash_key);
+                newopt->value = apr_pstrdup(pool, opt->value);
+                newopt->x_value = apr_pstrdup(pool, opt->x_value);
+                newopt->expanded = opt->expanded;
+                apr_hash_set(newsec->options, apr_pstrdup(pool, (const char*)optkey), optkeyLength, newopt);
+            }
+            apr_hash_set(newconfig->sections, apr_pstrdup(pool, (const char*)sectkey), sectkeyLength, newsec);
+        }
+        apr_hash_set(rethash, apr_pstrdup(pool, (const char*)ckey), ckeyLength, newconfig);
+    }
+
+    return rethash;
+}
---
+++ tags/version-1.7.15/src/TortoiseMerge/BottomView.cpp
@@ -0,0 +1,172 @@
+// TortoiseMerge - a Diff/Patch program
+
+// Copyright (C) 2006-2012 - TortoiseSVN
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+#include "StdAfx.h"
+#include "Resource.h"
+#include "AppUtils.h"
+
+#include "bottomview.h"
+
+IMPLEMENT_DYNCREATE(CBottomView, CBaseView)
+
+CBottomView::CBottomView(void)
+{
+    m_pwndBottom = this;
+    m_pState = &m_AllState.bottom;
+    m_nStatusBarID = ID_INDICATOR_BOTTOMVIEW;
+}
+
+CBottomView::~CBottomView(void)
+{
+}
+
+
+void CBottomView::AddContextItems(CIconMenu& popup, DiffStates state)
+{
+    const bool bShow = HasSelection() && (state != DIFFSTATE_UNKNOWN);
+    if (!bShow)
+        return;
+
+    popup.AppendMenuIcon(POPUPCOMMAND_USETHEIRBLOCK, IDS_VIEWCONTEXTMENU_USETHEIRBLOCK);
+    popup.AppendMenuIcon(POPUPCOMMAND_USEYOURBLOCK, IDS_VIEWCONTEXTMENU_USEYOURBLOCK);
+    popup.AppendMenuIcon(POPUPCOMMAND_USEYOURANDTHEIRBLOCK, IDS_VIEWCONTEXTMENU_USEYOURANDTHEIRBLOCK);
+    popup.AppendMenuIcon(POPUPCOMMAND_USETHEIRANDYOURBLOCK, IDS_VIEWCONTEXTMENU_USETHEIRANDYOURBLOCK);
+
+    CBaseView::AddContextItems(popup, state);
+}
+
+
+void CBottomView::UseBlock(CBaseView * pwndView, int nFirstViewLine, int nLastViewLine)
+{
+    if (!IsViewGood(pwndView))
+        return;
+    CUndo::GetInstance().BeginGrouping(); // start group undo
+
+    for (int viewLine = nFirstViewLine; viewLine <= nLastViewLine; viewLine++)
+    {
+        viewdata lineData = pwndView->GetViewData(viewLine);
+        lineData.ending = lineendings;
+        lineData.state = ResolveState(lineData.state);
+        SetViewData(viewLine, lineData);
+    }
+
+    int nRemovedLines = CleanEmptyLines();
+    SaveUndoStep();
+    UpdateViewLineNumbers();
+    SaveUndoStep();
+
+    CUndo::GetInstance().EndGrouping();
+
+    // final clean up
+    ClearSelection();
+    SetupAllViewSelection(nFirstViewLine, nLastViewLine - nRemovedLines);
+    BuildAllScreen2ViewVector();
+    SetModified();
+    RefreshViews();
+}
+
+void CBottomView::UseBothBlocks(CBaseView * pwndFirst, CBaseView * pwndLast)
+{
+    if (!IsViewGood(pwndFirst) || !IsViewGood(pwndLast))
+        return;
+    int nFirstViewLine = 0; // first view line in selection
+    int nLastViewLine  = 0; // last view line in selection
+
+    if (!GetViewSelection(nFirstViewLine, nLastViewLine))
+        return;
+
+    int nNextViewLine = nLastViewLine + 1; // first view line after selected block
+
+    CUndo::GetInstance().BeginGrouping(); // start group undo
+
+    // use (copy) first block
+    for (int viewLine = nFirstViewLine; viewLine <= nLastViewLine; viewLine++)
+    {
+        viewdata lineData = pwndFirst->GetViewData(viewLine);
+        lineData.ending = lineendings;
+        lineData.state = ResolveState(lineData.state);
+        SetViewData(viewLine, lineData);
+        if (!IsStateEmpty(pwndFirst->GetViewState(viewLine)))
+        {
+            pwndFirst->SetViewState(viewLine, DIFFSTATE_YOURSADDED); // this is improper (may be DIFFSTATE_THEIRSADDED) but seems not to produce any visible bug
+        }
+    }
+    SaveUndoStep();
+
+    // use (insert) last block
+    int nViewIndex = nNextViewLine;
+    for (int viewLine = nFirstViewLine; viewLine <= nLastViewLine; viewLine++, nViewIndex++)
+    {
+        viewdata lineData = pwndLast->GetViewData(viewLine);
+        lineData.state = ResolveState(lineData.state);
+        InsertViewData(nViewIndex, lineData);
+        if (!IsStateEmpty(pwndLast->GetViewState(viewLine)))
+        {
+            pwndLast->SetViewState(viewLine, DIFFSTATE_THEIRSADDED); // this is improper but seems not to produce any visible bug
+        }
+    }
+    SaveUndoStep();
+
+    // adjust line numbers in target
+    // we fix all line numbers to handle exotic cases
+    UpdateViewLineNumbers();
+    SaveUndoStep();
+
+    // now insert an empty block in both first and last
+    int nCount = nLastViewLine - nFirstViewLine + 1;
+    pwndLast->InsertViewEmptyLines(nFirstViewLine, nCount);
+    pwndFirst->InsertViewEmptyLines(nNextViewLine, nCount);
+    SaveUndoStep();
+
+    int nRemovedLines = CleanEmptyLines();
+    SaveUndoStep();
+
+    CUndo::GetInstance().EndGrouping();
+
+    // final clean up
+    ClearSelection();
+    SetupAllViewSelection(nFirstViewLine, 2*nLastViewLine - nFirstViewLine - nRemovedLines + 1);
+    BuildAllScreen2ViewVector();
+    SetModified();
+    pwndLast->SetModified();
+    pwndFirst->SetModified();
+    RefreshViews();
+}
+
+void CBottomView::UseViewBlock(CBaseView * pwndView)
+{
+    if (!IsViewGood(pwndView))
+        return;
+    int nFirstViewLine = 0; // first view line in selection
+    int nLastViewLine  = 0; // last view line in selection
+
+    if (!GetViewSelection(nFirstViewLine, nLastViewLine))
+        return;
+
+    return UseBlock(pwndView, nFirstViewLine, nLastViewLine);
+}
+
+void CBottomView::UseViewFile(CBaseView * pwndView)
+{
+    if (!IsViewGood(pwndView))
+        return;
+    int nFirstViewLine = 0;
+    int nLastViewLine = GetViewCount()-1;
+
+    return UseBlock(pwndView, nFirstViewLine, nLastViewLine);
+}
